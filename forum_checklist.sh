e06aece77b061054b2a36b87bb0c76ed353b6087
wonzoski <168494654+wonzoski@users.noreply.github.com>
2025-10-06 19:22:47 -0300

    Create forum_checklist.sh

---
+++ forum_checklist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+###################################################
+# HostGator Brasil LTDA - 2025
+#
+# Script version:
+# 1.0
+#
+# Last update:
+# 06/10/2025
+#
+###################################################
+# Author:
+#
+# Fabrício Wonzoski <fabricio.wonzoski@newfold.com>
+# Jr Advanced Support Analyst (N2)
+#
+###################################################
+#
+# Script to do a checklist
+#
+###################################################
