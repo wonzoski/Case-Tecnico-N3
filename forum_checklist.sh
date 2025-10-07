b8718f698e400fa975717b5cc8bd19320582b321
Fabricio Wonzoski <wonzoskifabricio@gmail.com>
2025-10-07 02:03:40 -0300

    Atualização 5

--- forum_checklist.sh
+++ forum_checklist.sh
@@ -23,16 +23,22 @@
 unset USER
 
 # DEFINE CORES
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
+BOLD='\033[1m'
+RED='\033[1;31m'  
+GREEN='\033[0;92m'
+CYAN='\033[1;36m'
+YELLOW='\033[38;5;11m'
+BLUE='\033[0;94m'
+ORANGE='\033[1;33m'
+PURPLE='\033[1;35m'
+NC='\033[0m' # No Color
 
 ##CONSTANTS
 MESERRO="${RED}[X]${NC}${BOLD}"
 MESINFO="${GREEN}[✓]${NC}${BOLD}" 
 MESINST="${YELLOW}[!]${NC}${BOLD}" 
 MESWARN="${PURPLE}[?]${NC}${BOLD}"
+SUBITEM="${BLUE} ► ${NC}${BOLD}"
 
 # Função de ajuda
 show_help() {
@@ -105,44 +111,35 @@ fi
 
 # Verifica se o usuário existe no userdomains (CORRIGIDO)
 if awk '{print $2}' /etc/trueuserdomains | grep -qw "$USER"; then
-	echo "✅ Usuário '$USER' validado com sucesso"
+	echo ""
+	echo "✅ Usuário '$USER' válido"
+	echo ""
 	return 0
 else
+	echo ""
 	echo "❌ Erro: Usuário '$USER' não encontrado no sistema"
 	echo "   Verifique se o nome de usuário está correto"
+	echo ""
 	exit 1
 fi
+
+INFO=$(quota -u "$USER" | awk '/^[[:space:]]*\/dev\// {print $1, $2, $3, $4, $6, $7, $8}')
+
+read PART USED QUOTA LIMIT FILES FQUOTA FLIMIT <<< "$INFO"
+
+USED_GB=$(awk "BEGIN {printf \"%.2f\", $USED/1048576}")
+LIMIT_GB=$(awk "BEGIN {printf \"%.2f\", $LIMIT/1048576}")
+
+
 }
 
 # Validação do usuário
 validate_user "$USER"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 echo ""
 echo "=== Verificações iniciais ==="
 echo "Sistema operacional $(cat /etc/redhat-release)"
-echo "A partição atual do usuário é ... numero atual de inodes em ... com limite ..."
+echo "A partição atual do usuário ${USER} é ${PART}, com uso de disco de ${USED_GB} GB (consumo atual) e limite de ${LIMIT_GB} GB (limite de disco atual), utilizando ${FILES} inodes (consumo atual de inodes) com limite de ${FLIMIT} inodes."
 echo "Ip atual deste usuário é ..."
 echo ""
 echo "=== VERIFICAÇÃO DE SERVIÇOS COMUNS ==="
