9e314fdca7028c9009e086ceb38112150501c775
Fabricio Wonzoski <wonzoskifabricio@gmail.com>
2025-10-07 19:23:41 -0300

    Atualização 5

--- forum_checklist.sh
+++ forum_checklist.sh
@@ -121,7 +121,7 @@ if awk '{print $2}' /etc/trueuserdomains | grep -qw "$USER"; then
 	echo ""
 	echo "✅ Usuário '$USER' válido"
 	echo "'${SUBITEM}' Partição atual do usuário: ${PART} atualmente com $(df -h | grep ${PART} | awk '{print $5}') de uso"
-	echo "${SUBITEM} uso de disco: ${USED_GB} GB e limite de ${LIMIT_GB} GB" 
+	echo -e "${SUBITEM} uso de disco: ${USED_GB} GB e limite de ${LIMIT_GB} GB" 
 	echo "${SUBITEM} Utilizando ${FILES} inodes com limite de ${FLIMIT} inodes."
 	echo ""
 	echo "=== VERIFICAÇÃO DE SERVIÇOS COMUNS ==="
@@ -149,4 +149,7 @@ fi
 
 # Validação do usuário
 validate_user "$USER"
+
+
+
 
