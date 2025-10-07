6e2fc84b00602840965daa7bcd0e8930548cb2dd
Fabricio Wonzoski <wonzoskifabricio@gmail.com>
2025-10-06 21:33:24 -0300

    Atualização 3

--- forum_checklist.sh
+++ forum_checklist.sh
@@ -20,13 +20,27 @@
 #
 ###################################################
 
+# DEFINE CORES
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # VARIÁVEIS LOCAIS
 echo "## Verificações iniciais ##"
 echo "Sistema operacional $(cat /etc/redhat-release}"
 echo "A partição atual do usuário é ... numero atual de inodes em ... com limite ..."
 echo "Ip atual deste usuário é ..."
-
-COMMON_SERVICES=(httpd mysqld cpanel pure-ftp sshd exim dovecot)
-for s in "${COMMON_SERVICES[@]}"; do
-	echo "Status do serviço: $s" $(systemctl is-active $s)
+echo "=== VERIFICAÇÃO DE SERVIÇOS COMUNS ==="
+		    
+COMMON_SERVICES="httpd mysqld cpanel pure-ftpd sshd exim dovecot"
+for service in $COMMON_SERVICES ; do
+		if systemctl is-active "$service" >/dev/null 2>&1;  then
+			status="Ativo"
+		else
+			status="Inativo"
+		fi
+		echo "Serviço: $service - Status? $status"
 done
+echo ""
+
