0989a87af435d77c30c642ebba2ec4e0fe2ee54d
Fabricio Wonzoski <wonzoskifabricio@gmail.com>
2025-10-07 20:21:23 -0300

    Atualização 5

--- forum_checklist.sh
+++ forum_checklist.sh
@@ -144,7 +144,7 @@ check_email() {
 echo ""
 echo -e "${MESINFO} Iniciando checklist para ${CYAN}E-mail${NC}"
 echo -e "${SUBITEM} Listando contas de e-mail do usuário..."
-echo -e "${PONTUACAO} uapi --user=$USER Email list_pops | egrep 'email.*@' | awk '{print $2}'"
+echo -e "${PONTUACAO} $(uapi --user=$USER Email list_pops | egrep 'email.*@' | awk '{print $2}')"
 # ...
 }
 
